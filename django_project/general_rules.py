3529bbf3eca45b91d16ee907d6c3753146660a18
Erick Augusto <erickaugusto542@gmail.com>
2021-06-02 00:07:31 +0000

    update

--- django_project/general_rules.py
+++ django_project/general_rules.py
@@ -2,7 +2,7 @@
  Variáveis que definem regras gerais para uso do site.
  Por favor, fazer o possível para modificar as regras apenas por este arquivo,
  caso a regra exista aqui.
-
+ 
  Ao adicionar uma nova variável de regra neste arquivo,
  talvez seja importante criar uma função que retorna essa variável
  no main_app_extras.py para que essa variável possa ser usada em templates.
