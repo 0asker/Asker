03e5fd35c62371afc20be2ad5b074cfabe3a4dcd
Erick Augusto <erickaugusto542@gmail.com>
2021-06-11 22:53:08 -0500

    update: sistema de URLs

--- django_project/urls.py
+++ django_project/urls.py
@@ -31,7 +31,16 @@ urlpatterns = [
   path('answer/like', views.like, name='like'), # quando uma resposta recebe um like, é usado esse padrão de URL.
   path('answer/choose', views.choose_best_answer, name="choose_best_answer"),
   path('delete_response', views.delete_response, name='delete_response'), # esse padrão de URL é usado para deletar respostas.
+  
   path('user/activity', views.activity, name='activity'),
+  path('user/me', views.activity, name='activity'),
+  path('user/you', views.activity, name='activity'),
+  path('activity', views.activity, name='activity'),
+  path('me', views.activity, name='activity'),
+  path('you', views.activity, name='activity'),
+  path('lnk', views.activity, name='activity'),
+  path('link', views.activity, name='activity'),
+  
   path('user/<str:username>', views.profile, name='profile'),
   path('user/<str:username>/edit', views.edit_profile, name='edit_profile'),
   path('user/<str:username>/block', views.block, name='block'), # esse padrão de URL é usado para um usuário bloquear outro.
