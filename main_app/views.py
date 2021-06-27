e1233d126c5bfe4ab6c90f2522aadee68f146131
maferdi <maur.as@live.com>
2021-06-27 19:51:57 -0300

    Fix - recomendadas sumindo ao responder

--- main_app/views.py
+++ main_app/views.py
@@ -159,7 +159,7 @@ para ser salva (no banco de dados).
 '''
 def save_answer(request):
 	if request.method != 'POST':
-		return HttpResponse('Proibido.');
+		return HttpResponse('Proibido.')
 
 	question_id = request.POST.get('question_id')
 	question = Question.objects.get(id=question_id)
