f3b5ec8d117f5d13cbf823573b9146904e3852af
Erick Augusto <erickaugusto542@gmail.com>
2021-07-16 15:04:09 -0500

    update

--- main_app/middlewares/ip_filter.py
+++ main_app/middlewares/ip_filter.py
@@ -19,7 +19,7 @@ class IpFilter:
 
 		ip = get_client_ip(request)
 		
-		if ip == '177.84.47.47' or ip == '104.244.72.168' or ip == '109.70.100.25' or ip == '179.176.97.151':
+		if ip == '177.84.47.47' or ip == '104.244.72.168' or ip == '109.70.100.25' or ip == '179.176.97.151' or ip == '191.33.210.17':
 			return HttpResponse('')
 
 		response = self.get_response(request)
