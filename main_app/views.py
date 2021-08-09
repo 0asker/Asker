b25a899a5955a82a68248f350c54d99300bcd140
Erick Augusto <erickaugusto542@gmail.com>
2021-08-08 19:26:01 -0500

    update

--- main_app/views.py
+++ main_app/views.py
@@ -155,7 +155,8 @@ def index(request):
 	else:
 		context['POPULAR'] = True
 
-	context['questoes_recentes'] = Question.objects.order_by('-id')[:15]
+	#context['questoes_recentes'] = Question.objects.order_by('-id')[:15]
+  context['questoes_recentes'] = Question.objects.order_by('-id')[80:100]
 
 	context['popular_questions'] = cache.get('p_questions')
 	if not context['popular_questions']:
