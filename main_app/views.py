2113ba737cd82992e54d75c4db14b76ee2f29a0d
Erick Augusto <erickaugusto542@gmail.com>
2021-08-17 23:13:33 -0500

    update

--- main_app/views.py
+++ main_app/views.py
@@ -170,7 +170,7 @@ def index(request):
     if request.user.is_authenticated:
         context['user_p'] = UserProfile.objects.get(user=request.user)
 
-    if random.randint(1, 10) == 10:
+    if random.randint(1, 10) == 40:
         context['SOCIAL_AD'] = True
 
     return render(request, 'index.html', context)
